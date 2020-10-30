024aa4db913cbf313642c3aaf6f5761c253fa9c2
gazm <gorgazm@gmail.com>
2020-10-30 03:56:37 -0400

    converted stat page html to reactjs elements

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,14 +1,25 @@
 import Layout from "../components/layout";
 import { fetchRPC, fetchCGinfo } from "../components/fetch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  OverlayTrigger,
+  Row,
+  Tooltip,
+  Card,
+  Col,
+  Container,
+} from "react-bootstrap";
+import { useRef } from "react";
+import { redirect } from "next/dist/next-server/server/api-utils";
+
+// Charting
 
 /* Fetch Functions */
 
 function getCGinfo(data: string) {
-  let result;
   //data=location of object as string ex.(".market_data.current_price.usd")
   const { cgInfo, isLoading, isError } = fetchCGinfo("haven");
+
   if (isError) return <h3 className="text-danger">🚨Error fetching data🚨</h3>;
   if (isLoading)
     return (
@@ -19,16 +30,16 @@ function getCGinfo(data: string) {
   if (data === ".market_data.current_price.usd")
     return (
       <span className="xhv-price">
-        💲 {cgInfo.market_data.current_price.usd.toLocaleString()}
+        💲 {cgInfo.market_data.current_price.usd.toFixed(2)}
       </span>
     );
   if (data === ".market_data.market_cap.usd")
     return <span>💲 {cgInfo.market_data.market_cap.usd.toLocaleString()}</span>;
   if (data === "price_change") {
-    result = {
-      "24h": +cgInfo.market_data.price_change_percentage_24h.toFixed(2),
-      "7d": +cgInfo.market_data.price_change_percentage_7d.toFixed(2),
-      "30d": +cgInfo.market_data.price_change_percentage_30d.toFixed(2),
+    const result = {
+      "24h": cgInfo.market_data.price_change_percentage_24h.toFixed(2) + "%",
+      "7d": cgInfo.market_data.price_change_percentage_7d.toFixed(2) + "%",
+      "30d": cgInfo.market_data.price_change_percentage_30d.toFixed(2) + "%",
     };
     return JSON.stringify(result);
   } else {
@@ -57,43 +68,44 @@ function getSupply(asset) {
 
 /* Components */
 
-function SmallCard(props) {
+function ReactCard(props) {
   return (
-    <div className="small-card col-sm" style={smallCard}>
-      <div className="card" style={card}>
-        <div className="card-header" style={cardHeader}>
-          {props.title}
-        </div>
-        <div className="card-body">{props.children}</div>
-      </div>
-    </div>
+    <Col sm style={smallCard}>
+      <Card style={card}>
+        <Card.Header style={cardHeader}>{props.title}</Card.Header>
+        <Card.Body>
+          <Card.Text>{props.children}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
 
 const stats = () => (
   <Layout>
-    <div style={style}>
-      <div className="row">
-        <SmallCard title="XHV Price">
+    <Container className="text-center">
+      <Row>
+        <ReactCard title="XHV Price" style={card}>
           <OverlayTrigger
             overlay={
               <Tooltip id="tooltip-disabled">
                 {getCGinfo("price_change")}
               </Tooltip>
             }
+            popperConfig={popper.settings}
           >
             <span>{getCGinfo(".market_data.current_price.usd")}</span>
           </OverlayTrigger>
-        </SmallCard>
-        <SmallCard title="MarketCap">
+        </ReactCard>
+        <ReactCard title="MarketCap">
           <span>{getCGinfo(".market_data.market_cap.usd")}</span>
-        </SmallCard>
-        <SmallCard title="xUSD Circulating">
+        </ReactCard>
+        <ReactCard title="xUSD Circulating">
           <span>{getSupply("xUSD")}</span>
-        </SmallCard>
-      </div>
-      <div className="row"></div>
-    </div>
+        </ReactCard>
+      </Row>
+      <Row></Row>
+    </Container>
   </Layout>
 );
 
@@ -105,7 +117,7 @@ const style = {
 };
 
 const smallCard = {
-  opacity: ".9",
+  fontSize: "x-large",
 };
 
 const cardHeader = {
@@ -119,6 +131,24 @@ const cardHeader = {
 const card = {
   border: "1px solid #2d3135",
   backgroundColor: "#36393F",
+};
+
+const popper = {
+  settings: {
+    modifiers: [
+      {
+        name: "preventOverflow",
+        options: {
+          mainAxis: false,
+          altAxis: false,
+          padding: 0,
+          elementContext: "reference",
+          rootBoundary: "document",
+          tether: false,
+        },
+      },
+    ],
+  },
 };
 
 /*   End Styles */
