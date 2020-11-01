89fb1e6a72ce41d69c3a07e4d8be341170cc72bd
gazm <gorgazm@gmail.com>
2020-11-01 12:55:45 -0500

    add fa icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
-import "../node_modules/bootstrap/scss/bootstrap.scss";
 import "../styles/main.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircle, faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircle,
+  faCircleNotch,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
-library.add(faCircle, faCircleNotch);
+library.add(faCircle, faCircleNotch, faExclamationTriangle);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
